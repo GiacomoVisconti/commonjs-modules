4d8c001abde49ff16cec184d5d3021ca4a5d9b37
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-25 14:16:12 +0200

    add name function to name.js and export it

--- names.js
+++ names.js
@@ -1,3 +1,10 @@
+/**
+ *  This function takes a first name and a last name as arguments
+ *  and returns an object containing both names.
+ * @param {string} first_name 
+ * @param {string} last_name 
+ * @returns 
+ */
 function name(first_name, last_name){
     const full_name = {
         first_name: first_name,
@@ -7,3 +14,6 @@ function name(first_name, last_name){
     return full_name
 }
 
+
+//Export name function
+export default name;
