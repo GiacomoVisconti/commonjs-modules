efb5cb2ac16e66a6712510a8715a90da0d91645b
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-25 14:18:40 +0200

    add function to hobbies.js and export it

---
+++ hobbies.js
@@ -0,0 +1,14 @@
+/**
+ *  This function takes three hobbies as arguments and returns them in an array.
+ * @param {string} hobby_one 
+ * @param {string} hobby_two 
+ * @param {string} hobby_three 
+ * @returns {Array} hobbies
+ */
+function hobbies(hobby_one, hobby_two, hobby_three){
+    const hobbies = [hobby_one, hobby_two, hobby_three];
+    return hobbies;
+}
+
+//Export hobbies function
+export default hobbies;
